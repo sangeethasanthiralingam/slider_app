073fa34e9eaa095755ce970aca2994671365e203
janu1499 <januvr14@gmail.com>
2023-12-07 16:52:51 +0530

    change is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './style/Style.css';
 import image1 from './img/park1.jpg';
@@ -8,8 +7,11 @@ import image3 from './img/park3.jpg';
 import image4 from './img/park4.jpg';
 import leftarrow from './img/leftarrow.png';
 import rightarrow from './img/rightarrow.png';
-import Contact from './components/Contact';
-import Testimonial from './components/Testimonial';
+import Navbar from './components/Navbar';
+import './style/mystyle.css';
+import Footer from './components/Footer';
+
+
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
@@ -18,21 +20,6 @@ function App() {
     setCurrentPage(page);
   };
 
-  let currentContent = null;
-
-  if (currentPage === 'home') {
-    currentContent = (
-      <>
-        {/* Your home page content */}
-        <button onClick={() => handleNavigation('contact')}>Go to Contact</button>
-        <button onClick={() => handleNavigation('testimonial')}>Go to Testimonial</button>
-      </>
-    );
-  } else if (currentPage === 'contact') {
-    currentContent = <Contact />;
-  } else if (currentPage === 'testimonial') {
-    currentContent = <Testimonial />;
-  }
   const images = [image1, image2, image3, image4];
   const textContent = [
     'Welcome to Jet Park!',
@@ -52,22 +39,13 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className='nav-bar'>
-        <h4 className='logo-name'>Jet Park</h4>
-        <ul className='nav-header'>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>About US</a></li>
-          <li><a href='#'>Services</a></li>
-          <li><a href='#' onClick={() => handleNavigation('testimonial')}>Testimonial</a></li>
-          <li><a href='#' onClick={() => handleNavigation('contact')}>Contact US</a></li>
-          <button className='login'>Login</button>
-        </ul>
-      </div>
-      {/* Render content based on currentPage */}
-      <div className='content'>
-        {currentContent}
-      </div>
+     <div className="container">
+       
+       {/* Render content based on currentPage */}
+       <div className='content'>
+         {/* {currentContent} */}
+       </div>
+      <Navbar/>
       <div className='slider_container'>
         <div>
           <img
@@ -92,22 +70,9 @@ function App() {
           />
         </div>
       </div>
-      <div className='footer'>
-        <div className='footer-logo'>
-          <p className="copyright">© 2023 Jet Park. All rights reserved.</p>
-        </div>
-        <div className='footer-links'>
-          <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About US</a></li>
-            <li><a href='#'>Services</a></li>
-            <li><a href='#'>Testimonial</a></li>
-            <li><a href='#'>Contact US</a></li>
-          </ul>
-        </div>
-      </div>
+    <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './components/Contact';
+import Testimonial from './components/Testimonial';
+import Login from './components/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/testimonial' element={<Testimonial/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
